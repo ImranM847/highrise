9c9d1853ab4e6109bdfc6f199a925c669f9ed189
Dmitri Popov <dmpop@linux.com>
2018-02-12 13:20:59 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Highrise
+
+Highrise is a Bash shell script for generating super resolution images from a stack of low-resolution photos. Script is based on [https://github.com/pixlsus/Scripts/tree/master/superres](https://github.com/pixlsus/Scripts/tree/master/superres).
+
+# Dependencies
+
+- ImageMagic
+- Hugin tool
+- ExifTool
+
+To install the required packages on openSUSE, use the following command:
+
+    sudo zypper in ImageMagick hugin exiftool
+
+## Installation
+
+```
+git clone git@github.com:dmpop/highrise.git
+sudo cp highrise.sh /usr/local/bin/highrise
+sudo chown root:root /usr/local/bin/highrise
+sudo chmod 755 /usr/local/bin/highrise
+```
+
+## Usage
+
+    highrise -e [EXT] -d [DIR]
+
+Replace `[EXT]` with the file extension of the source photos (e.g., *JPG* or *jpg*) and `[DIR]` with the absolute path to the directory containing source photos.
+
+## Problems?
+
+Please report bugs and issues in the [Issues](https://github.com/dmpop/highrise/issues) section.
+
+## Contribute
+
+If you've found a bug or have a suggestion for improvement, open an issue in the [Issues](https://github.com/dmpop/highrise/issues) section.
+
+To add a new feature or fix issues yourself, follow the steps below.
+
+1. Fork the project's repository repository
+2. Create a feature branch using the `git checkout -b new-feature` command
+3. Add your new feature or fix bugs and run the `git commit -am 'Add a new feature'` command to commit changes
+4. Push changes using the `git push origin new-feature` command
+5. Submit a pull request
+
+## Author
+
+Dmitri Popov [dmpop@linux.com](mailto:dmpop@linux.com)
+
+## License
+
+The [GNU General Public License version 3](http://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ highrise.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# Bash shell script for generating a super resolution images from stacks of lower resolution photos.
+# Based on https://github.com/pixlsus/Scripts/tree/master/superres
+# 
+
+# Check whether the required packages are installed
+if [ ! -x "$(command -v convert)" ] || [ ! -x "$(command -v align_image_stack)" ] || [ ! -x "$(command -v exiftool)" ]; then
+    echo "Install ImageMagick, Hugin, and ExifTool."
+    exit 1
+fi
+
+# Usage prompt
+usage(){
+cat <<EOF
+$0 [OPTIONS]
+$0 creates a super resolution image from a stack of lower resolution photos
+Usage:
+  $0 -e <extention> -d <directory>
+Options:
+  -e --ext       File extension (e.g., JPG or jpg)
+  -d --dir       Absolute path to the directory containing source photos
+EOF
+  exit 1
+}
+
+# Specify options
+OPTS=`getopt -o e:d: -l ext:dir: -- "$@"`
+[[ $# -eq 0 ]] && usage
+eval set -- "$OPTS"
+
+# Obtain paramter values
+while true; do
+  case "$1" in
+    -e | --ext ) ext="$2"; shift 2;;
+    -d | --dir ) dir="$2"; shift 2;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+# Check whether the required paramteters are specified
+if [ -z "$ext" ]; then
+ echo "File extension is not specified"
+exit 1
+fi
+if [ -z "$dir" ]; then
+ echo "Source directory is not specified"
+exit 1
+fi
+
+# Create scratch directory for temporary file
+mkdir -p $dir/resized || exit 1
+cd $dir
+
+# Upscale images by 200% (4x more pixels) and copy them to the scratch directory
+convert *.$ext -resize 200% $dir/resized/%04d.jpg
+cd $dir/resized || exit 1
+
+# Auto-align resized images and crop them to aligned area
+align_image_stack --use-given-order --distortion -C -z -a aligned --corr=0.8 -t 1 -c 100 -v *.jpg
+cd $dir
+
+# Obtain file name of the first file in the working directory
+file=$(ls *.$ext | sort -n | head -1)
+filename="${file%.*}"
+
+# Calculate average at each new pixel. Save the resulting file using the obtained file name
+# Copy EXIF metadata from the first file into the resulting file
+convert $dir/resized/aligned* -evaluate-sequence mean $filename-mean.$ext
+exiftool -overwrite_original -tagsfromfile $file "-all:all>all:all" $filename-mean.$ext
+
+# Calculate median at each new pixel. Save the resulting file using the obtained file name
+# Copy EXIF metadata from the first file into the resulting file
+convert $dir/resized/aligned* -evaluate-sequence median $filename-median.$ext
+exiftool -overwrite_original -tagsfromfile $file "-all:all>all:all" $filename-mean.$ext
+
+# Remove the scratch directory
+rm -rf $dir/resized
+
+echo "All done. You'll find the resulting $filename-mean.$ext and $filename-mean.$ext files in the working directory."
+
+exit 0
